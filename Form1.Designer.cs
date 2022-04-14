006d8241ddf27c73595d6cb0d88830ef4c795bd6
iwanami <aozorakyozai@gmail.com>
2022-04-14 20:03:40 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace WindowsFormsAppBasic
+{
+    partial class setting
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.L1 = new System.Windows.Forms.Button();
+            this.R1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // L1
+            // 
+            this.L1.Location = new System.Drawing.Point(1, 1);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(80, 80);
+            this.L1.TabIndex = 0;
+            this.L1.Text = "<";
+            this.L1.UseVisualStyleBackColor = true;
+            this.L1.Click += new System.EventHandler(this.L1_Click);
+            // 
+            // R1
+            // 
+            this.R1.Location = new System.Drawing.Point(80, 1);
+            this.R1.Name = "R1";
+            this.R1.Size = new System.Drawing.Size(80, 80);
+            this.R1.TabIndex = 1;
+            this.R1.Text = ">";
+            this.R1.UseVisualStyleBackColor = true;
+            this.R1.Click += new System.EventHandler(this.R1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(159, 1);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(512, 80);
+            this.textBox1.TabIndex = 2;
+            // 
+            // setting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1017, 83);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.R1);
+            this.Controls.Add(this.L1);
+            this.Name = "setting";
+            this.Text = "setting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button L1;
+        private System.Windows.Forms.Button R1;
+        private System.Windows.Forms.TextBox textBox1;
+        
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsAppBasic
+{
+    public partial class setting : System.Windows.Forms.Form
+    {
+        Excel.Application elxApp;
+        Excel.Workbook wb;
+        Excel.Worksheet ws;
+        Excel.Range rng;
+        // コンストラクタ
+        public setting()
+        {
+            InitializeComponent();
+            MessageBox.Show("指定したフォルダからファイル名を取得する");
+            // 指定してフォルダの中のファイルを探す
+            string[] files = Directory.GetFiles(@"C:\testFolder\", "*");
+            try
+            {
+                // 検索したファイルを表示する
+                foreach (string f in files)
+                {
+                    MessageBox.Show(f);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public void L1_Click(object sender, EventArgs e)
+        {
+            
+            // Excel起動
+            elxApp = new Excel.Application();
+            // Excelのウィンドウを表示・非表示
+            elxApp.Visible = true;
+            elxApp.Application.DisplayAlerts = true;
+
+            // Bookを追加
+            //elxapp.Application.Workbooks.Add(Type.Missing);
+            //csWorkbook = new Excel.Workbook();
+
+            /******** Excel WorkBook ********/
+            // C直下にtestFolderを作成し、testSc.xlsを作成する
+            // パスを指定してエクセルを開く
+            // Type.Missing：特に指定しない場合
+            wb = elxApp.Workbooks.Open(@"C:\testFolder\testCs", Type.Missing);
+
+            /******** Sheet ********/
+
+            // Bookを追加
+            //Excel.Workbook wb;
+            //wb = csWorkbook.Application.Workbooks.Add(Type.Missing);
+
+            // シート選択
+            ws = wb.Worksheets[2];
+
+            // セルの値を取得
+            rng = ws.Cells[1, 1];
+            MessageBox.Show(rng.Value);
+
+            // Formを終了する
+            //Application.Exit();
+        }
+
+        private void R1_Click(object sender, EventArgs e)
+        {
+            // Excel保存
+            //wb.SaveAs(@"C:\testFolder\testCs");
+        }
+        private void Form1_Clik(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+        
+    }
+}
